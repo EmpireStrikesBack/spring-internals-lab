cb83e451722a2b3e18cd4cff2a4bdd2b4b14080a
tvallois <theo.vallois@hotmail.fr>
2026-02-27 16:19:03 +0100

    level1 : private fields/methods inspectio + modifiers(access/invoke member) + params/return types dynamically inspected + dynamical new Class instance creation

--- level1-reflection/src/main/java/com/example/ReflectionDemo.java
+++ level1-reflection/src/main/java/com/example/ReflectionDemo.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class ReflectionDemo {
@@ -37,6 +39,42 @@ public class ReflectionDemo {
                     ", Modifiers:" + Modifier.toString(field.getModifiers())
                 );
             } 
+
+
+            // step 3 : inspect all declared methods 
+
+            System.out.println("\n== METHODS ==");
+
+            // getDeclaredMethods() returns all declared methods in a class (including private ones)
+            Method[] methods = personClass.getDeclaredMethods();
+
+            for (Method method : methods) {
+                System.out.print(
+                    "Name:" + method.getName() +
+                    ", Return Type:" + method.getReturnType().getSimpleName() +
+                    ", Modifiers: " + Modifier.toString(method.getModifiers())
+                );
+
+                // print params if any
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                if (parameterTypes.length > 0) {
+                    System.out.print(", Params : ");
+                    for (Class<?> param : parameterTypes) {
+                        System.out.print(param.getSimpleName() + " ");
+                    }
+                }
+                System.out.println();
+            }
+
+            // step 4 : dynamic instanciation 
+
+            Constructor<?> defaultConstructor = personClass.getConstructor(); //public no-arg constructor 
+            Object personInstance = defaultConstructor.newInstance(); // dynamicaaly create instance 
+            
+            // use getters to print default values 
+            System.out.println("\n== DYNAMIC INSTANCE==");
+            System.out.println("Default name: " + ((Person) personInstance).getName());
+            System.out.println("Default age: " + ((Person) personInstance).getAge());
         } catch (Exception e) {
             // Reflection API throw checked exceptions (ClassNotFound etc...)
             e.printStackTrace();
