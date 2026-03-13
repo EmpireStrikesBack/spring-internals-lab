6112e79f11fa3927e50e6fc0d34746cc5497e4af
tvallois <theo.vallois@hotmail.fr>
2026-03-13 11:29:04 +0100

    feature/level2 : Dynamically create the proxy object

---
+++ level2-proxies/src/main/java/com/example/ProxyDemo.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.lang.reflect.Proxy;
+
+/*
+Step 4 : create & test the dynamic proxy
+- Proxy.newProxyInstance() : dynamically generates a class that implements the Service interface
+*/
+
+public class ProxyDemo {
+    public static void main(String[] args) {
+        // create the real service object 
+        Service realService = new ServiceImpl();
+
+        /*
+        create proxy instance 
+
+        params:
+        - ClassLoader
+        - Interfaces implemented by proxy
+        - Invocationhandler that intercepts calls 
+        */
+       Service proxyService = (Service) Proxy.newProxyInstance
+       (
+        Service.class.getClassLoader(),
+        new Class[]{Service.class},
+        new LoggingHandler(realService)
+       );
+
+       //call method through proxy
+       proxyService.execute();
+    }
+}
