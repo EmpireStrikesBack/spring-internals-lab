0718948e0cdadb96d680b4c8deb3a7a27e1ed831
tvallois <theo.vallois@hotmail.fr>
2026-03-13 10:54:03 +0100

    feature/level2 : define the target interface

--- level2-proxies/src/main/java/com/example/Service.java
+++ level2-proxies/src/main/java/com/example/Service.java
@@ -1,5 +1,12 @@
 package com.example;
 
-public class Service {
-    
+/*
+Step 1 : define a service interface 
+- Dynamic proxies operate on interfaces 
+- The proxy object will implement this interface & intercept method calls
+*/
+
+public interface Service {
+    // Method that'll be intercepted by the proxy 
+    void execute();    
 }
