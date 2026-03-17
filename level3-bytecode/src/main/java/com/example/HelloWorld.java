37d72aa1037a7b7bd1f3e1283117e0421ecfb36b
tvallois <theo.vallois@hotmail.fr>
2026-03-17 11:34:09 +0100

    feature/level3: inspect JVM bytecode + inspect full class file structure

---
+++ level3-bytecode/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/*
+Step 1 : simple class whose bycote we'll inspect 
+*/
+
+public class HelloWorld {
+    public void sayHello() {
+        System.out.println("Hello, there to inspect your bytecode");
+    }
+}
