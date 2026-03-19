b986472f9bda1e74ed864d165f08a089acd6ee3b
tvallois <theo.vallois@hotmail.fr>
2026-03-19 18:09:04 +0100

    feature/level4 : @Component annotaiton

--- level4-mini-di/src/main/java/com/example/Component.java
+++ level4-mini-di/src/main/java/com/example/Component.java
@@ -1,5 +1,23 @@
 package com.example;
 
-public class Component {
-    
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/*
+Step 1a : @Component annotation 
+ - @Retention(RUNTIME) : critical --> annotation is stripped without it
+    - isAnnotationPresent() would retrun false at runtime
+    - Spring uses RUNTIME retention on all its meta-annotations for this reason
+ - @Target(TYPE) : resticts this annotaiton to class declarations only 
+    - Prevents accidental use on fields or methods 
+*/
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Component {
+    // optionnal logical bean name : mirrors Spring's @Component("myBean")
+    String value() default "";
 }
