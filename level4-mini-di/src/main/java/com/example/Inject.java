4562b3ad6b6a4e62076059a3423ca5bd0a465f61
tvallois <theo.vallois@hotmail.fr>
2026-03-19 18:14:04 +0100

    feature/level4 : @Inject annotation

--- level4-mini-di/src/main/java/com/example/Inject.java
+++ level4-mini-di/src/main/java/com/example/Inject.java
@@ -1,5 +1,22 @@
 package com.example;
 
-public class Inject {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+Step 1b : @Inject annotation
+ - @Target(FIELD) : applies to instance fields only.
+    - The injecetor scans getDeclaredFields() & looks for this annotation 
+    - Equivalent to Spring's @Autowire in fields 
+ - Security : field injection requires setAccessible(true)
+    - Same mechanism used in Lvl1 step 6 to bypass encapsulation
+    - This is why injection frameworks need ReflectPermission in security managers
+*/
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Inject {
     
 }
