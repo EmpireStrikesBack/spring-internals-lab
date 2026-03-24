e31c822fe95cb8f65f018c53133ec94c91d3527d
tvallois <theo.vallois@hotmail.fr>
2026-03-24 18:13:57 +0100

    feature/level4 : NotAComponent

--- level4-mini-di/src/main/java/com/example/NotAComponent.java
+++ level4-mini-di/src/main/java/com/example/NotAComponent.java
@@ -1,5 +1,14 @@
 package com.example;
 
+/*
+Plain class : intentonally not annoted with @Component 
+    - Used in the scanner demo to confirm that ComponentScanner correctly ignores classes without the annotation
+    - Attempting to inject this type from any @Inject field would throw an "Unsatisfied dependency" RuntimeException from DependencyInjector
+*/
+
+
 public class NotAComponent {
-    
+    public String label() {
+        return "I'm not managed by the container";
+    }
 }
