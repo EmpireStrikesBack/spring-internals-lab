0a26d304fa8c0b8d461d9b6150af299cdbe3b6e7
tvallois <theo.vallois@hotmail.fr>
2026-03-24 18:20:25 +0100

    feature/level4 : CircularA & CircularB cycle

--- level4-mini-di/src/main/java/com/example/CircularA.java
+++ level4-mini-di/src/main/java/com/example/CircularA.java
@@ -1,5 +1,22 @@
 package com.example;
 
+/*
+Step 8 :  Circular dependency demo : Circular 1 injects circularB
+
+CircularA --> CircularB --> CircularA (cycle)
+
+Always fails with constructor injection 
+ - JVM can't instantiate either class without the other existing first
+
+With field injection : Spring detects it via the currently Creating set
+- Throws a RuntimeException with a clear message 
+
+*/
+
+@Component 
 public class CircularA {
-    
+    @Inject 
+    private CircularB b; // requires CircularB
+
+    public String ping() {return "A->" + b.pong();}
 }
--- level4-mini-di/src/main/java/com/example/CircularB.java
+++ level4-mini-di/src/main/java/com/example/CircularB.java
@@ -1,5 +1,15 @@
 package com.example;
 
+/*
+Step 8 : Circular dependency demo : CircularB injects CircularA
+
+CircularB --> CircularA --> CircularB (cycle)
+*/
+
+@Component 
 public class CircularB {
-    
+    @Inject
+    private CircularA a; // need CircularA - closes the cycle 
+
+    public String pong() {return "B->" + a.ping();}
 }
